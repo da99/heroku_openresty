4d075b72f5cb6d2f8976b39575b4b4c78aaa2a0e
da99 <i-hate-spam-1234567@mailinator.com>
2016-12-23 13:11:39 -0600

    Various: fixes

--- install.openresty.sh
+++ install.openresty.sh
@@ -1,4 +1,8 @@
 
+# Based on: lua_setup install openresty
+#           and on:
+#           https://github.com/geoffleyland/heroku-buildpack-openresty/blob/master/bin/compile
+
 latest-ver () {
   git ls-remote -t https://github.com/openresty/openresty | cut -d'/' -f 3 | sort -r | grep -P '^v[0-9\.]+$' | head -n 1 | cut -d'v' -f2
 }
@@ -41,6 +45,9 @@ upgrade-openresty () {
   echo "-----> Downloading $LATEST_ARCHIVE... "
 
 	cd "$CACHE_DIR"
+  echo "-----> in cache directory: $PWD ["
+  ls -1
+  echo "-----> ]"
 
   if [[ ! -d ${LATEST_DIR} ]]; then
     if [[ ! -s $LATEST_ARCHIVE ]]; then
@@ -49,11 +56,13 @@ upgrade-openresty () {
 		tar -xvf ${LATEST_ARCHIVE} >/dev/null || { rm $LATEST_ARCHIVE; upgrade-openresty "$PREFIX"; exit 0; }
 	fi
 
-  set "-x"
   cd $LATEST_DIR
+  echo "-----> in openresty source directory: $PWD"
 
   local +x PROCS="$(grep -c '^processor' /proc/cpuinfo)"
 
+  # NOTE: /sbin in PATH is needed because OpenResty
+  # compilation requires ldconfig in PATH.
   PATH="$PATH:/sbin" ./configure                      \
     --prefix="$PREFIX"             \
     --with-http_iconv_module       \
@@ -67,5 +76,8 @@ upgrade-openresty () {
 
   make
   make install
+
+  set "-x"
+  ln -s "$PREFIX"/luajit/lib/libluajit-5.1.so.2.1.0 "$PREFIX"/luajit/lib/libluajit-5.1.so.2
 
 } # === end function
